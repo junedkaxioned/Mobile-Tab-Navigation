7813fe5e3b7a38205210a2cf435617e09a9e71b1
junedkaxioned <junedk@axioned.com>
2022-10-04 13:26:57 +0530

    Js Completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-/* Author: Zishan Ansari */
+var images = document.querySelectorAll('.image');
+var tabLists = document.querySelectorAll('.tab-list');
+
+// function for NavList
+tabLists.forEach(function (list, idx) {
+  list.addEventListener('click', function () {
+
+    // Loop for remove class
+    for (var i = 0; i < tabLists.length; i++) {
+      tabLists[i].classList.remove('active');
+      images[i].classList.remove('show-image');
+    }
+
+    this.classList.add('active');
+    images[idx].classList.add('show-image');
+  })
+})
